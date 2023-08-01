14ddd1364b8b27393e0ac9730ca44f39639408af
Kalucza Gábor <113378113+kaluczadzsi@users.noreply.github.com>
2023-08-01 18:41:31 +0200

    Revisiting Functions & Parameters

--- javascript-refresher/assets/scripts/app.js
+++ javascript-refresher/assets/scripts/app.js
@@ -1,7 +1,14 @@
 // import apiKey from './util.js';
-import { apiKey, abc as content } from './util.js';
+// import { apiKey, abc as content } from './util.js';
 
 // import * as util from './util.js';
 
 // console.log(util.default);
-console.log(content);
+// console.log(content);
+
+// function greetUser(username, message = 'Hello!') {
+//   return `Hi, I am ${username} ${message}`;
+// }
+
+// const res = greetUser('Max', 'nice to meet you');
+// console.log(res);
