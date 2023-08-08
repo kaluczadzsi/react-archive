6df28cc0996eec88a5b718239d5fdbf55245a720
Kalucza Gábor <113378113+kaluczadzsi@users.noreply.github.com>
2023-08-08 17:30:48 +0200

    Alternative ways of passing & receiving / handling props

--- react-basics-and-working-with-components/src/App.js
+++ react-basics-and-working-with-components/src/App.js
@@ -31,7 +31,6 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <p>This is also visible</p>
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
