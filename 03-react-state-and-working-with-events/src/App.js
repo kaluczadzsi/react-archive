7c24add19ef0d54e0bc58a3098f51ab29e7479db
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-09-03 15:12:29 +0200

    Adding Form Inputs

--- 03-react-state-and-working-with-events/src/App.js
+++ 03-react-state-and-working-with-events/src/App.js
@@ -1,3 +1,4 @@
+import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
 import React from 'react';
 
@@ -29,18 +30,18 @@ function App() {
     },
   ];
 
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h2', {}, "Let's get started!"),
-    React.createElement(Expenses, { items: expenses })
-  );
-  // return (
-  //   <div>
-  //     <h2>Let's get started!</h2>
-  //     <Expenses items={expenses} />
-  //   </div>
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
   // );
+  return (
+    <div>
+      <NewExpense />
+      <Expenses items={expenses} />
+    </div>
+  );
 }
 
 export default App;
